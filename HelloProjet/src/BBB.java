7a216a12670ee5482f3d601093bce62cb2abc5f6
peibingmei <824955247@qq.com>
2019-06-16 20:42:35 +0800

    v

--- HelloProjet/src/BBB.java
+++ HelloProjet/src/BBB.java
@@ -1,6 +1,5 @@
 public class BBB {
     public static void main(String[] args) {
-        System.out.println("abc");
         System.out.println("bbb");
         System.out.println("eee");
         System.out.println("ggg");
